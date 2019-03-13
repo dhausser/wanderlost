4b1b04a97454a74f716d3c6c7fbbbb2e9c5e58a3
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-03-13 21:45:11 +0100

    fixed color

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -7,8 +7,8 @@ const theme = {
   red: '#FF0000',
   black: '#393939',
   grey: '#3A3A3A',
-  lightgrey: 'E1E1E1',
-  offwhite: 'EDEDED',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
   maxWidth: '1000px',
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
@@ -26,12 +26,11 @@ const Inner = styled.div`
 
 injectGlobal`
   @font-face {
-    font-family: 'radnika-next';
-    src: url('/static/radnikanext-medium-webfont.woff2')
-    format('woff2');
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
     font-weight: normal;
     font-style: normal;
-  }}
+  }
   html {
     box-sizing: border-box;
     font-size: 10px;
@@ -40,11 +39,11 @@ injectGlobal`
     box-sizing: inherit;
   }
   body {
-    padding:  0;
+    padding: 0;
     margin: 0;
     font-size: 1.5rem;
     line-height: 2;
-    font-family: 'radnika-next';
+    font-family: 'radnika_next';
   }
   a {
     text-decoration: none;

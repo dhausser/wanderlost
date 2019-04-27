04020df030b4119de86ecebc3b1b2d63bddbec6d
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-04-27 18:14:20 +0200

    items useQuery with react apollo hooks

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
-import Item from './Item';
-import Pagination from './Pagination';
-import { perPage } from '../config';
+import React from 'react'
+import { useQuery } from 'react-apollo-hooks'
+import gql from 'graphql-tag'
+import styled from 'styled-components'
+import Item from './Item'
+import Pagination from './Pagination'
+import { perPage } from '../config'
 
 export const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
@@ -17,11 +17,11 @@ export const ALL_ITEMS_QUERY = gql`
       largeImage
     }
   }
-`;
+`
 
 const Center = styled.div`
   text-align: center;
-`;
+`
 
 const ItemsList = styled.div`
   display: grid;
@@ -29,33 +29,28 @@ const ItemsList = styled.div`
   grid-gap: 60px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
-`;
+`
 
-export default class Items extends Component {
-  render() {
-    return (
-      <Center>
-        <Pagination page={this.props.page} />
-        <Query
-          query={ALL_ITEMS_QUERY}
-          variables={{
-            skip: this.props.page * perPage - perPage,
-          }}
-        >
-          {({ data, error, loading }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error: {error.message}</p>;
-            return (
-              <ItemsList>
-                {data.items.map(item => (
-                  <Item key={item.id} item={item} />
-                ))}
-              </ItemsList>
-            );
-          }}
-        </Query>
-        <Pagination page={this.props.page} />
-      </Center>
-    );
+export default function Items(props) {
+  const { data, loading, error } = useQuery(ALL_ITEMS_QUERY, {
+    variables: { skip: props.page * perPage - perPage },
+  })
+
+  if (loading) {
+    return <p>Loading...</p>
   }
+  if (error) {
+    return <p>Error: {error.message}</p>
+  }
+
+  return (
+    <Center>
+      <ItemsList>
+        {data.items.map(item => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ItemsList>
+      <Pagination page={props.page} />
+    </Center>
+  )
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,32 +1,35 @@
-import App, { Container } from 'next/app';
-import { ApolloProvider } from 'react-apollo';
-import Page from '../components/Page';
-import withData from '../lib/withData';
+import App, { Container } from 'next/app'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloProviderHooks } from 'react-apollo-hooks'
+import Page from '../components/Page'
+import withData from '../lib/withData'
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+    let pageProps = {}
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
+      pageProps = await Component.getInitialProps(ctx)
     }
     // this exposes the  query to the user
-    pageProps.query = ctx.query;
-    return { pageProps };
+    pageProps.query = ctx.query
+    return { pageProps }
   }
 
   render() {
-    const { Component, apollo, pageProps } = this.props;
+    const { Component, apollo, pageProps } = this.props
 
     return (
       <Container>
         <ApolloProvider client={apollo}>
-          <Page>
-            <Component {...pageProps} />
-          </Page>
+          <ApolloProviderHooks client={apollo}>
+            <Page>
+              <Component {...pageProps} />
+            </Page>
+          </ApolloProviderHooks>
         </ApolloProvider>
       </Container>
-    );
+    )
   }
 }
 
-export default withData(MyApp);
+export default withData(MyApp)

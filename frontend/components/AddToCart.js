2d06c2322e272c18e6b55ad92fbfd42d36d182db
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-04-28 20:03:43 +0200

    displaying cart item and total

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useMutation } from 'react-apollo-hooks'
 import { gql } from 'apollo-boost'
+import { CURRENT_USER_QUERY } from './User'
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -13,12 +14,27 @@ const ADD_TO_CART_MUTATION = gql`
 
 export default function(props) {
   const { id } = props
+  const [loading, setLoading] = useState(false)
   const addToCart = useMutation(ADD_TO_CART_MUTATION, {
     variables: { id },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
   })
+
+  useEffect(() => {
+    async function add() {
+      await addToCart()
+      setLoading(false)
+    }
+    add()
+  }, [loading])
+
+  function handleAddToCart() {
+    setLoading(true)
+  }
+
   return (
-    <button type="button" onClick={addToCart}>
-      Add to Cart 🛒
+    <button type="button" disabled={loading} onClick={handleAddToCart}>
+      Add{loading && 'ing'} to Cart 🛒
     </button>
   )
 }
--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useQuery, useMutation } from 'react-apollo-hooks'
 import { gql } from 'apollo-boost'
+import User from './User'
 import CartStyles from './styles/CartStyles'
 import Supreme from './styles/Supreme'
 import CloseButton from './styles/CloseButton'
 import SickButton from './styles/SickButton'
+import CartItem from './CartItem'
+import calcTotalPrice from '../lib/calcTotalPrice'
+import formatMoney from '../lib/formatMoney'
 
 export const LOCAL_STATE_QUERY = gql`
   query {
@@ -20,21 +24,37 @@ export const TOGGLE_CART_MUTATION = gql`
 
 export default () => {
   const toggleCart = useMutation(TOGGLE_CART_MUTATION)
-  const { data } = useQuery(LOCAL_STATE_QUERY)
+  const {
+    data: { cartOpen },
+  } = useQuery(LOCAL_STATE_QUERY)
   return (
-    <CartStyles open={data.cartOpen}>
-      <header>
-        <CloseButton onClick={toggleCart} title="close">
-          &times;
-        </CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You have __ Items in your cart.</p>
-      </header>
-
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
+    <User>
+      {({ data: { me } }) => {
+        if (!me) return null
+        return (
+          <CartStyles open={cartOpen}>
+            <header>
+              <CloseButton onClick={toggleCart} title="close">
+                &times;
+              </CloseButton>
+              <Supreme>{me.name}'s Cart</Supreme>
+              <p>
+                You have {me.cart.length} Item{me.cart.length === 1 ? '' : 's'}{' '}
+                in your cart.
+              </p>
+            </header>
+            <ul>
+              {me.cart.map(cartItem => (
+                <CartItem key={cartItem.id} cartItem={cartItem} />
+              ))}
+            </ul>
+            <footer>
+              <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
+        )
+      }}
+    </User>
   )
 }

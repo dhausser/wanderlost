cf8c96a6156d16b0d5d574e0c9e589cc2eeebe41
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-04-28 18:03:05 +0200

    apollo local state queries and mutations

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,20 +1,42 @@
 import React from 'react'
+import { useQuery, useMutation } from 'react-apollo-hooks'
+import { gql } from 'apollo-boost'
 import CartStyles from './styles/CartStyles'
 import Supreme from './styles/Supreme'
 import CloseButton from './styles/CloseButton'
 import SickButton from './styles/SickButton'
 
-export default () => (
-  <CartStyles open>
-    <header>
-      <CloseButton title="close">&times;</CloseButton>
-      <Supreme>Your Cart</Supreme>
-      <p>You have __ Items in your cart.</p>
-    </header>
+export const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`
 
-    <footer>
-      <p>$10.10</p>
-      <SickButton>Checkout</SickButton>
-    </footer>
-  </CartStyles>
-)
+export const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`
+
+export default () => {
+  const toggleCart = useMutation(TOGGLE_CART_MUTATION)
+  const { data } = useQuery(LOCAL_STATE_QUERY)
+  return (
+    console.log(data) || (
+      <CartStyles open={data.cartOpen}>
+        <header>
+          <CloseButton onClick={toggleCart} title="close">
+            &times;
+          </CloseButton>
+          <Supreme>Your Cart</Supreme>
+          <p>You have __ Items in your cart.</p>
+        </header>
+
+        <footer>
+          <p>$10.10</p>
+          <SickButton>Checkout</SickButton>
+        </footer>
+      </CartStyles>
+    )
+  )
+}
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,37 +1,43 @@
-import Link from 'next/link';
-import NavStyles from './styles/NavStyles';
-import User from './User';
-import Signout from './Signout';
+import Link from 'next/link'
+import { useMutation } from 'react-apollo-hooks'
+import { TOGGLE_CART_MUTATION } from './Cart'
+import NavStyles from './styles/NavStyles'
+import User from './User'
+import Signout from './Signout'
 
-const Nav = () => (
-  <User>
-    {({ data: { me } }) => (
-      <NavStyles>
-        <Link href="/items">
-          <a>Shop</a>
-        </Link>
-        {me && (
-          <>
-            <Link href="/sell">
-              <a>Sell</a>
-            </Link>
-            <Link href="/orders">
-              <a>Orders</a>
-            </Link>
-            <Link href="/me">
-              <a>Account</a>
-            </Link>
-            <Signout />
-          </>
-        )}
-        {!me && (
-          <Link href="/signup">
-            <a>Signup</a>
+export default function() {
+  const toggleCart = useMutation(TOGGLE_CART_MUTATION)
+  return (
+    <User>
+      {({ data: { me } }) => (
+        <NavStyles>
+          <Link href="/items">
+            <a>Shop</a>
           </Link>
-        )}
-      </NavStyles>
-    )}
-  </User>
-);
-
-export default Nav;
+          {me && (
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+              <Signout />
+              <button type="button" onClick={toggleCart}>
+                My Cart
+              </button>
+            </>
+          )}
+          {!me && (
+            <Link href="/signup">
+              <a>Signup</a>
+            </Link>
+          )}
+        </NavStyles>
+      )}
+    </User>
+  )
+}

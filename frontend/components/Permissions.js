3f669c3d64b8517616074e9f19d3ff535ff54f00
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-04-28 13:25:26 +0200

    updating permissions in local state

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { useQuery } from 'react-apollo-hooks'
 import gql from 'graphql-tag'
+import PropTypes from 'prop-types'
 import Error from './ErrorMessage'
 import Table from './styles/Table'
 import SickButton from './styles/SickButton'
@@ -24,7 +26,7 @@ const ALL_USERS_QUERY = gql`
   }
 `
 
-export default function Permissions(props) {
+export default function Permissions() {
   const { loading, error, data } = useQuery(ALL_USERS_QUERY)
   if (loading) return <p>Loading...</p>
   return (
@@ -45,7 +47,7 @@ export default function Permissions(props) {
           </thead>
           <tbody>
             {data.users.map(user => (
-              <User user={user} />
+              <UserPermission user={user} key={user.id} />
             ))}
           </tbody>
         </Table>
@@ -54,16 +56,41 @@ export default function Permissions(props) {
   )
 }
 
-function User(props) {
+function UserPermission(props) {
+  console.log(props.user.permissions)
+  const [permissions, setPermissions] = useState(props.user.permissions)
+
+  function handlePermissionChange(e) {
+    const checkbox = e.target
+    // take a copy of the current permission
+    let updatedPermissions = [...permissions]
+    // figure if we need to remove or add this permission
+    if (checkbox.checked) {
+      // add it in!
+      updatedPermissions.push(checkbox.value)
+    } else {
+      updatedPermissions = updatedPermissions.filter(
+        permission => permission !== checkbox.value,
+      )
+    }
+    setPermissions(updatedPermissions)
+    console.log(updatedPermissions)
+  }
+
   const { user } = props
   return (
     <tr>
       <td>{user.name}</td>
       <td>{user.email}</td>
       {possiblePermissions.map(permission => (
-        <td>
+        <td key={permission}>
           <label htmlFor={`${user.id}-permission-${permission}`}>
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={permissions.includes(permission)}
+              value={permission}
+              onChange={handlePermissionChange}
+            />
           </label>
         </td>
       ))}
@@ -72,4 +99,13 @@ function User(props) {
       </td>
     </tr>
   )
+}
+
+UserPermission.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    id: PropTypes.string,
+    permission: PropTypes.array,
+  }).isRequired,
 }

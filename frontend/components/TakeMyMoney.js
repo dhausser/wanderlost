de21cb5636cf75f7e3a91375c601f65c877e2254
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-05-02 23:02:12 +0200

    charging cards on the server side

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -9,30 +9,59 @@ import calcTotalPrice from '../lib/calcTotalPrice'
 import Error from './ErrorMessage'
 import User, { CURRENT_USER_QUERY } from './User'
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)
 }
 
-function onToken(res) {
+function onToken(res, createOrder) {
   console.log('On Token Called!')
   console.log(res.id)
+  // Manually call the mutation once we have the stripe token
+  createOrder({
+    variables: {
+      token: res.id,
+    },
+  }).catch(err => {
+    alert(err.message)
+  })
 }
 
 export default props => (
   <User>
     {({ data: { me } }) => (
-      <StripeCheckout
-        amount={calcTotalPrice(me.cart)}
-        name="Sick Fits"
-        description={`Order of ${totalItems(me.cart)} items!`}
-        // image={me.cart[0].item && me.cart[0].item.image}
-        stripeKey="pk_test_zywrqZUXI6crPwbzolFxAyF100AF2Wh0HA"
-        currency="USD"
-        email={me.email}
-        token={res => onToken(res)}
+      <Mutation
+        mutation={CREATE_ORDER_MUTATION}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
       >
-        {props.children}
-      </StripeCheckout>
+        {createOrder => (
+          <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="Sick Fits"
+            description={`Order of ${totalItems(me.cart)} items!`}
+            // image={me.cart[0].item && me.cart[0].item.image}
+            stripeKey="pk_test_zywrqZUXI6crPwbzolFxAyF100AF2Wh0HA"
+            currency="USD"
+            email={me.email}
+            token={res => onToken(res, createOrder)}
+          >
+            {props.children}
+          </StripeCheckout>
+        )}
+      </Mutation>
     )}
   </User>
 )

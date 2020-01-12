e1b3918467adf21b1c0926cef6d3816e6a2aa786
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2020-01-12 23:02:00 +0100

    readme udpate

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -25,31 +25,35 @@ function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
-async function onToken(res, checkout) {
-  const router = useRouter();
-  NProgress.start();
-  // Manually call the mutation once we have the stripe token
-  const order = await checkout({
-    variables: {
-      token: res.id,
-    },
-  }).catch((err) => {
-    alert(err.message);
-  });
-  router.push({
-    pathname: '/order',
-    query: { id: order.data.checkout.id },
-  });
-}
 
 function TakeMyMoney({ children }) {
   const authenticatedUser = useUser();
   const { toggleCart } = useCart();
+  const router = useRouter();
+
   const [checkout, { loading }] = useMutation(CREATE_ORDER_MUTATION, {
     update: () => toggleCart(false),
     refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
+
+  async function onToken(res) {
+    NProgress.start();
+    // Manually call the mutation once we have the stripe token
+    const order = await checkout({
+      variables: {
+        token: res.id,
+      },
+    }).catch((err) => {
+      alert(err.message);
+    });
+    router.push({
+      pathname: '/order',
+      query: { id: order.data.checkout.id },
+    });
+  }
+
   if (loading) return null;
+
   return (
     <StripeCheckout
       amount={calcTotalPrice(authenticatedUser.cart)}
@@ -63,7 +67,7 @@ function TakeMyMoney({ children }) {
       stripeKey="pk_test_zywrqZUXI6crPwbzolFxAyF100AF2Wh0HA"
       currency="USD"
       email={authenticatedUser.email}
-      token={(res) => onToken(res, checkout)}
+      token={(res) => onToken(res)}
     >
       {children}
     </StripeCheckout>

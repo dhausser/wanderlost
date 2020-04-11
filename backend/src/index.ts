af877aae3a7cf959daa1318b8a501bf26a71bf49
Davy Hausser <davy.hausser@gmail.com>
2020-04-12 00:02:33 +0200

    datamodel migrated to postgres db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,23 +2,34 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Davy',
-      email: 'davy@prisma.io',
-      password: 'dogs',
-      permissions: 'ADMIN',
-    },
-  })
-
-  // const deletedUser = await prisma.user.delete({
-  //   where: { email: 'sarah@prisma.io' },
-  // })
-
-  // const allUsers = await prisma.user.findMany()
+async function getAllUsers() {
   const allUsers = await prisma.user.findMany()
   console.log(allUsers)
+  return allUsers
+}
+
+async function createUser() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Davy Hausser',
+      email: 'dhausser@prisma.io',
+      permissions: 'ADMIN',
+      password: '123456',
+    },
+  })
+  return user
+}
+
+async function deleteUser() {
+  const deletedUser = await prisma.user.delete({
+    where: { email: 'dhausser@prisma.io' },
+  })
+}
+
+async function main() {
+  await createUser()
+  // await deleteUser()
+  await getAllUsers()
 }
 
 main()

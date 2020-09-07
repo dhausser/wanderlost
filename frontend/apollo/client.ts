86d6ff93b5b0b719b3ab44da05c89b76256cdda8
Davy Hausser <davy.hausser@gmail.com>
2020-09-07 22:20:24 +0200

    :spakles: succesfully migrated to api routes

--- frontend/apollo/client.ts
+++ frontend/apollo/client.ts
@@ -1,52 +1,47 @@
-import { useMemo } from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  NormalizedCacheObject,
-} from "@apollo/client";
-import { endpoint, prodEndpoint } from "../config.json";
+import { useMemo } from 'react'
+import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
 
-let apolloClient: ApolloClient<NormalizedCacheObject | null>;
+let apolloClient: ApolloClient<NormalizedCacheObject | null>
 
 function createIsomorphLink() {
-  // if (typeof window === "undefined") {
-  //   const { SchemaLink } = require("@apollo/client/link/schema");
-  //   const { schema } = require("./schema");
-  //   const { createContext } = require("./context");
-  //   return new SchemaLink({ schema, context: createContext() });
-  // }
-  const { HttpLink } = require("@apollo/client/link/http");
+  if (typeof window === 'undefined') {
+    const { SchemaLink } = require('@apollo/client/link/schema')
+    const { schema } = require('./schema')
+    const { createContext } = require('./context')
+    return new SchemaLink({ schema, context: createContext() })
+  }
+  const { HttpLink } = require('@apollo/client/link/http')
   return new HttpLink({
-    uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
-    credentials: "same-origin",
-  });
+    uri: '/api/graphql',
+    credentials: 'same-origin',
+  })
 }
 
 function createApolloClient() {
   return new ApolloClient({
-    ssrMode: typeof window === "undefined",
+    ssrMode: typeof window === 'undefined',
     link: createIsomorphLink(),
     cache: new InMemoryCache(),
-  });
+  })
 }
 
 export function initializeApollo(initialState = null) {
-  const _apolloClient = apolloClient ?? createApolloClient();
+  const _apolloClient = apolloClient ?? createApolloClient()
 
   // If your page has Next.js data fetching methods that use Apollo Client, the initial state
   // gets hydrated here
   if (initialState) {
-    _apolloClient.cache.restore(initialState);
+    _apolloClient.cache.restore(initialState)
   }
   // For SSG and SSR always create a new Apollo Client
-  if (typeof window === "undefined") return _apolloClient;
+  if (typeof window === 'undefined') return _apolloClient
   // Create the Apollo Client once in the client
-  if (!apolloClient) apolloClient = _apolloClient;
+  if (!apolloClient) apolloClient = _apolloClient
 
-  return _apolloClient;
+  return _apolloClient
 }
 
 export function useApollo(initialState: null | undefined) {
-  const store = useMemo(() => initializeApollo(initialState), [initialState]);
-  return store;
+  const store = useMemo(() => initializeApollo(initialState), [initialState])
+  return store
 }
---
+++ frontend/apollo/types.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from '@prisma/client'
+
+export enum Permissions {
+  ADMIN,
+  ITEMCREATE,
+  ITEMDELETE,
+  ITEMUPDATE,
+  PERMISSIONUPDATE,
+  USER,
+}
+
+export interface UserInput {
+  email: string
+  password: string
+  name: string
+  permissions: Permissions[]
+}
+
+export interface ItemInput {
+  id: string
+  title: string
+  description: string
+  image: string
+  largeImage: string
+  price: number
+  user: any
+}
+
+export interface Item {
+  id: string
+  title: string
+  description: string
+  image: string
+  largeImage: string
+  price: number
+  user: any
+  createdAt: String
+  updatedAt: String
+}
+
+export interface CartItem extends Item {
+  quantity: number
+  item: Item
+}
+
+export interface Context {
+  req: {
+    userId: string | undefined
+    headers: {
+      authorization: string | undefined
+    }
+  }
+  res: any
+  prisma: PrismaClient
+  user: any
+}
+
+export interface TokenInterface {
+  email: string
+  name: string
+  userId: number
+}
+
+export interface Pagination {
+  offset: number
+  limit: number
+}
+
+export interface ItemConnection {
+  cursor: number
+  hasMore: boolean
+  items: Item[]
+}

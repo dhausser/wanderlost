8a1fe6df7689ecd9fe564afba2ba664dd343337a
Davy Hausser <davy.hausser@gmail.com>
2020-09-12 17:10:09 +0200

    :recycle: fixed missing user Id in orders deleteCartItem resolver

--- apollo/resolvers/mutations/deleteCartItem.ts
+++ apollo/resolvers/mutations/deleteCartItem.ts
@@ -1,9 +1,9 @@
 import { Context } from '../../types'
 
-export async function deleteCartItem(_: any, { id }: { id: string }, { user, prisma }: Context) {
+export async function deleteCartItem(_: any, { id }: { id: string }, { req, prisma }: Context) {
   const cartItem = await prisma.cartItem.findOne({ where: { id } })
   if (!cartItem) throw new Error('No CartItem found!')
-  if (cartItem.userId !== user.id) throw new Error('The item must be in your own cart')
+  if (cartItem.userId !== req.userId) throw new Error('The item must be in your own cart')
   const deleted = await prisma.cartItem.delete({ where: { id } })
   return deleted
 }

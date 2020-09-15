1ba900fb4a38a85621932bf7ad3d7b9ee3386d52
Davy Hausser <davy.hausser@gmail.com>
2020-09-15 15:15:37 +0200

    :construction: adding type Date with GraphQLDate from graphql-iso-date

--- apollo/schema/index.ts
+++ apollo/schema/index.ts
@@ -1,12 +1,15 @@
-import { makeSchema } from '@nexus/schema'
+import { makeSchema, asNexusMethod } from '@nexus/schema'
+import { GraphQLDate } from 'graphql-iso-date'
 import path from 'path'
 
 import * as Item from './Item'
 import * as Order from './Order'
 import * as User from './User'
 
+export const GQLDate = asNexusMethod(GraphQLDate, 'date')
+
 export const schema = makeSchema({
-  types: [Item, Order, User],
+  types: [Item, Order, User, GraphQLDate],
   typegenAutoConfig: {
     contextType: 'ctx.Context',
     sources: [

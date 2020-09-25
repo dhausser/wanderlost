140d9053bfd38ba3772a5eeea99f6e189560d7a9
Davy Hausser <davy.hausser@gmail.com>
2020-09-25 09:14:45 +0200

    :label: strict type safety for paginatedResults.ts

--- graphql/utils/paginatedResults.ts
+++ graphql/utils/paginatedResults.ts
@@ -1,14 +1,25 @@
+interface Item {
+  cursor: string
+}
+
+interface PaginatedResults {
+  after: string
+  pageSize: number
+  results: Array<Item>
+  getCursor: (item: Item) => string | null
+}
+
 const paginatedResults = ({
   after: cursor,
   pageSize = 20,
   results,
   // can pass in a function to calculate an item's cursor
   getCursor = () => null,
-}: any) => {
+}: PaginatedResults) => {
   if (pageSize < 1) return []
 
   if (!cursor) return results.slice(0, pageSize)
-  const cursorIndex = results.findIndex((item) => {
+  const cursorIndex = results.findIndex((item: Item) => {
     // if an item has a `cursor` on it, use that, otherwise try to generate one
     const itemCursor = item.cursor ? item.cursor : getCursor(item)
 

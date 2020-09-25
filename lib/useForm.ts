08070e45a6e7b28c5ec2977358a77a693b9a069e
Davy Hausser <davy.hausser@gmail.com>
2020-09-25 12:56:58 +0200

    🚨 removing eslint typescript rules and warnings for useForm.tsx

--- lib/useForm.ts
+++ lib/useForm.ts
@@ -1,7 +1,7 @@
 import { useState, ChangeEvent, Dispatch, SetStateAction } from 'react'
 
 // TODO: strict type safety for inputs
-type Inputs = Record<string, any>
+type Inputs = Record<string, string | number>
 
 type Event = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
 
@@ -26,13 +26,10 @@ async function uploadFile({ value, inputs, setInputs }: FileUploadProps) {
   // TODO: configure upload present for wanderlost instead of sickfits
   data.append('upload_preset', 'sickfits')
 
-  const res = await fetch(
-    'https://api.cloudinary.com/v1_1/davyhausser/image/upload',
-    {
-      method: 'POST',
-      body: data,
-    }
-  )
+  const res = await fetch('https://api.cloudinary.com/v1_1/davyhausser/image/upload', {
+    method: 'POST',
+    body: data,
+  })
   const file = await res.json()
   setInputs({
     ...inputs,
@@ -44,18 +41,22 @@ async function uploadFile({ value, inputs, setInputs }: FileUploadProps) {
 export default function useForm(initial: Inputs): Form {
   const [inputs, setInputs] = useState(initial)
 
-  function handleChange(e: {
-    target: { files?: any; name?: any; type?: any; value?: any }
-  }) {
+  function handleChange(e: Event) {
     const { name, type } = e.target
     let { value } = e.target
-    if (type === 'number') {
-      value = parseInt(value, 10)
-    }
+
+    // if (type === 'number') {
+    // value = parseInt(value, 10)
+    // }
+
     if (type === 'file') {
-      [value] = e.target.files
+      // value = e.target.files[0]
+      const target = e.target as HTMLInputElement
+      const file: File = (target.files as FileList)[0]
+      value = String(file)
       uploadFile({ value, inputs, setInputs })
     }
+
     setInputs({
       ...inputs,
       [name]: value,
@@ -67,9 +68,7 @@ export default function useForm(initial: Inputs): Form {
   }
 
   function clearForm() {
-    const blankState = Object.fromEntries(
-      Object.entries(inputs).map(([key]) => [key, ''])
-    )
+    const blankState = Object.fromEntries(Object.entries(inputs).map(([key]) => [key, '']))
     setInputs(blankState)
   }
 

0bf601750f60a7bd4efd4d37355e8622ea92e230
Davy Hausser <davy.hausser@gmail.com>
2020-10-01 12:14:21 +0200

    :white_check_mark: test suite for Dog

--- test/Dog.test.tsx
+++ test/Dog.test.tsx
@@ -21,47 +21,50 @@ const mocks = [
   },
 ]
 
-afterEach(() => {
-  cleanup()
-})
+describe('Dog', () => {
+  // automatically unmount and cleanup DOM after the test is finished.
+  afterEach(() => {
+    cleanup()
+  })
 
-it('renders without error', () => {
-  render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <Dog name="Buck" />
-    </MockedProvider>
-  )
-})
+  it('renders without error', () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Dog name="Buck" />
+      </MockedProvider>
+    )
+  })
 
-// it('should render loading state initially', () => {
-//   const { getByText } = render(
-//     <MockedProvider mocks={[]}>
-//       <Dog name="Buck" />
-//     </MockedProvider>
-//   )
+  it('should render loading state initially', async () => {
+    const { findByText } = render(
+      <MockedProvider mocks={[]}>
+        <Dog name="Buck" />
+      </MockedProvider>
+    )
 
-//   expect(getByText('Loading...')).toBeInTheDocument
-// })
+    const text = await findByText('Loading...')
+    expect(text).toBeInTheDocument
+    // expect(getByText('Loading...')).toBeInTheDocument
+  })
 
-it('should render dog', async () => {
-  const dogMock = {
-    request: {
-      query: GET_DOG_QUERY,
-      variables: { name: 'Buck' },
-    },
-    result: {
-      data: { dog },
-    },
-  }
+  it('should render dog', async () => {
+    const dogMock = {
+      request: {
+        query: GET_DOG_QUERY,
+        variables: { name: 'Buck' },
+      },
+      result: {
+        data: { dog },
+      },
+    }
 
-  const { findByText } = render(
-    <MockedProvider mocks={[dogMock]} addTypename={false}>
-      <Dog name="Buck" />
-    </MockedProvider>
-  )
+    const { findByText } = render(
+      <MockedProvider mocks={[dogMock]} addTypename={false}>
+        <Dog name="Buck" />
+      </MockedProvider>
+    )
 
-  const text = await findByText('Buck is a poodle')
-  console.log(text)
-
-  expect(text).toBeInTheDocument
+    const text = await findByText('Buck is a poodle')
+    expect(text).toBeInTheDocument
+  })
 })
